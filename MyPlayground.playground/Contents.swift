7fa581c8ae2195ef8f903c30a021b25ce2b625b1
An Le <an.le@notabasement.com>
2019-03-19 14:38:37 +0700

    Add example

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,14 +4,34 @@ protocol FckinEqualtable {
 }
 
 extension FckinEqualtable where Self: Equatable {
+    
     func equalToAll(_ array: [Self]) -> Bool {
         return array.reduce(true, {$0 && ($1 == self)})
     }
     
     func equalToOne(_ array: [Self]) -> Bool {
-        return array.contains(self)
+        return array.reduce(false, {$0 || ($1 == self)})
     }
 }
 
 
 extension String: FckinEqualtable { }
+
+
+let string = "string"
+let array = ["string1", "string2", "string3"]
+
+if !string.equalToOne(array) {
+    print("YESS")
+} else {
+    print("NOOO")
+}
+
+
+if !(string == "string1" || string == "string2") {
+    // Do something
+    print("-----YES")
+} else {
+    print("-----NO")
+}
+
